62ad305d0d0c01cb1ac23845fe552055431c2782
David Deyen <daviddeyen@gmail.com>
2021-02-07 15:06:05 -0600

    updates

--- scripts/applications-common.sh
+++ scripts/applications-common.sh
@@ -8,6 +8,8 @@ brew cask install google-backup-and-sync
 # Terminal
 
 brew cask install iterm2
+brew install zsh-completions
+brew install zsh-autosuggestions
 brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
 brew install starship
@@ -34,3 +36,5 @@ brew cask install ccleaner
 brew cask install tiles
 
 brew install mas
+
+brew install geekbench
